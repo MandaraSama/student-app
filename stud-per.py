3d1e40dd13ef925771f05344c98128986c42a925
Mandar Aarondekar <realmandara@gmail.com>
2025-10-08 16:47:42 +0530

    I have just updated stud-per.py file

--- stud-per.py
+++ stud-per.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import scikit-learn
+import sklearn
 import sklearn.preprocessing
 from sklearn.preprocessing import StandardScaler,LabelEncoder
 
